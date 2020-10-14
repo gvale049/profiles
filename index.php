d7eb4795c1223dc6b0a83bab8efd78a6aad8fb9a
Gabriel Valencia <ma301gv@gold.ac.uk>
2020-10-14 21:35:28 +0100

    index file with html

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php require_once "bootstrap.php"; ?>
+        <title> Gabriel Valencia - Index page </title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Gabriel Valencia's Resume Registery</h1>
+            <p> <a href="login.php"> Please Log in</a> </p>
+        </div>
+    </body>
+</html>
