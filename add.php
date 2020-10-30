47dbf211ca18846988b2adc55ff371e765c24bb9
Gabriel Valencia <ma301gv@gold.ac.uk>
2020-10-30 16:56:55 +0000

    strating update 30/10/20

--- add.php
+++ add.php
@@ -96,11 +96,38 @@
             <input type="text" name="headline" size="80"/></p>
         <p> Summary: </br>
             <textarea name="summary" rows="8" cols="80"></textarea></p>
+        <p> Position: <input type="submit" id="addPos" value="+"></p>
+        <div id="position_fields"></div>
         <p>
             <input type="submit" value="Add">
             <input type="submit" name="cancel" value="Cancel">
         </p>
     </form>
+    <script>
+        countPos = 0;
+
+        $(document).ready(function(){
+            window.console && console.log('Document ready called');
+            $('#addPos').click(function(event){
+                // http://api.jquery.com/event.preventdefault/
+                event.preventDefault();
+                if (countPos >= 9) {
+                    alert("Maximum of nine position entries exceeded");
+                    return;
+                }
+                countPos++;
+                window.console && console.log("Adding position "+ countPos);
+                $('#position_fields').append(
+                    '<div id="position'+countPos+'"> \
+                    <p>Year: <input type="text" name="year'+countPos+'" value="" /> \
+                    <input type="button" value="-" \
+                        onclick="$(\'#position'+countPos+'\').remove();return false;"></p> \
+                        <textarea name="desc'+countPos+'" rows="8" cols="80"></textarea>\
+                    </div>'
+                );
+            });
+        });
+    </script>
 </div>
 </body>
 </html>
--- bootstrap.php
+++ bootstrap.php
@@ -5,4 +5,11 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
 <!-- Custom styles for this template -->
-<link href="starter-template.css" rel="stylesheet">
+<!--<link href="starter-template.css" rel="stylesheet"> -->
+
+<!-- JQuery CDN – Latest Stable Versions -->
+<script 
+    src="https://code.jquery.com/jquery-3.5.1.js" 
+    integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" 
+    crossorigin="anonymous">
+</script>
